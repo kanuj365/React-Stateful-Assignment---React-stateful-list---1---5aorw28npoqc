3ba2599c7ac54d6ffca044c01cca9b6b6c146630
kanuj365 <68420715+kanuj365@users.noreply.github.com>
2022-04-05 20:43:49 +0530

    React state

--- src/components/App.js
+++ src/components/App.js
@@ -1,39 +1,59 @@
-import React from 'react'
-
-import '../styles/App.css';
+import React from "react";
+import { useEffect, useState } from "react";
+import "../styles/App.css";
 
 const data = {
-  '2018': [
-    'Avengers 1',
-    'Now you see me',
-    'Fast and Furious 7'
+  "2018": ["Avengers 1", "Now you see me", "Fast and Furious 7"],
+  "2019": ["Avengers 2", "Now you see me 2", "Fast and Furious 8"],
+  "2020": [
+    "Final Avengers Movie(Iron man dies)",
+    "Now you finally used Lenskart",
+    "Covid Came"
   ],
-  '2019': [
-    'Avengers 2',
-    'Now you see me 2',
-    'Fast and Furious 8'
-  ],
-  '2020': [
-    'Final Avengers Movie(Iron man dies)',
-    'Now you finally used Lenskart',
-    'Covid Came'
-  ],
-  '2021': [
-    'Covid Returns',
-  ],
-  '2022': [
-    'Adventures of Saiman',
-    'Adventures of Shaktiman'
-  ]
-}
+  "2021": ["Covid Returns"],
+  "2022": ["Adventures of Saiman", "Adventures of Shaktiman"]
+};
 const App = () => {
+  const years = Object.keys(data);
+  const [currentYear, setCurrentYear] = useState(null);
+  const [movieList, setMovieList] = useState([]);
 
+  const handleChange = (e) => {
+    setCurrentYear(e.target.value);
+  };
+  function handleList() {
+    if (currentYear !== null) {
+      setMovieList(data[currentYear]);
+    }
+  }
+
+  useEffect(() => {
+    handleList();
+  }, [currentYear]);
   return (
-    <div id="main">
-      
+    <div className="App" id="main">
+      <select onChange={handleChange}>
+        <option value="null"> </option>
+        {years.map((year) => (
+          <option key={year} value={year}>
+            {year}
+          </option>
+        ))}
+      </select>
+      <div id="selected-year">
+        {currentYear ? (
+          <p>Selected year-{currentYear}</p>
+        ) : (
+          <p>No year selected</p>
+        )}
+      </div>
+      <ul>
+        {movieList.map((movie, index) => (
+          <li key={index}>{movie}</li>
+        ))}
+      </ul>
     </div>
-  )
-}
-
+  );
+};
 
 export default App;
